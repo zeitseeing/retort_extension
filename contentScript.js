657ecc441f22fd8e9dbf42b0efe8085aaabe3f41
zeitseeing <zeitseeing@protonmail.com>
2023-01-16 18:40:41 +0100

    More efficiently find screenName
    Using find instead of filter and extracting this snippet into a method.

--- contentScript.js
+++ contentScript.js
@@ -5,19 +5,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     const textEl = replyToTweet.querySelector("div[data-testid=\"tweetText\"]");
 
     // get screename
-    // find the script containing the screenname
-    const scripts = document.querySelectorAll('script');
-    const scriptContainingScreenName = Array.from(scripts).filter(script => script.text.startsWith("window.__INITIAL_STATE__"));
-
-    let screenName;
-
-    if (scriptContainingScreenName.length == 0) {
-      screenName = "handle not found"
-    }
-    else {
-      const scriptContent = scriptContainingScreenName[0].text
-      screenName = scriptContent.match('screen_name":"(.*?)",.')[1] // regex to find screenname screen_name":"(.*?)",.
-    }
+    const screenName = getScreenName();
 
     // get parent tweet url
     const parentTweetUrl = document.querySelector("link[rel=canonical]").getAttribute("href")
@@ -25,3 +13,18 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     // send tweet content, screenName, parentTweetUrl to popup.js
     sendResponse({ text: textEl.textContent, screenName: screenName, parentTweetUrl: parentTweetUrl });
 });
+
+const getScreenName = () => {
+    // find the script containing the screenname
+    const scripts = document.querySelectorAll('script');
+    scriptTextContent = Array.from(scripts)
+        .find(script => script.textContent.startsWith("window.__INITIAL_STATE__"))
+        .textContent;
+
+    if (!!scriptTextContent) {
+        return scriptTextContent.match('screen_name":"(.*?)",.')[1] // regex to find screenname screen_name":"(.*?)",.
+    }
+    else {
+        return "username not found"
+    };
+};
